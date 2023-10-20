6a91f12106d15c550dd5f4652aee4e07fde94d50
rojayachase <rojayachase@gmail.com>
2023-10-19 21:14:12 -0400

    productionready

--- password_generator/settings.py
+++ password_generator/settings.py
@@ -82,11 +82,10 @@ CORS_ALLOWED_ORGINS = True
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
