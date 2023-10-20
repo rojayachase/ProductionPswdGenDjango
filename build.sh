71dd74a7cccf844067251c8600db3f8cb8c24596
rojayachase <rojayachase@gmail.com>
2023-10-19 21:06:50 -0400

    productionready

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #To do anything you need to get the application build / install dependencies 
 
 #Install Dependencies
+pip install django
 pip install --upgrade pip
 pip install -r requirements.txt
 
